2e5cdfcfb944c19a4ab6b608b11475987ff52a76
Akash Jain <ajcodes.tech@gmail.com>
2024-09-07 00:42:20 +0530

    fest : working test

--- application/src/test/kotlin/Test.kt
+++ application/src/test/kotlin/Test.kt
@@ -1,17 +1,9 @@
 import org.junit.jupiter.api.Test
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
-
 
 class Test {
 
     @Test
     fun test() {
-        assertTrue { true }
-    }
-
-    @Test
-    fun test2() {
-        assertFalse { false }
+        assert(true)
     }
 }
